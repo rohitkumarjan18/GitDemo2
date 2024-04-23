3b17b3ce96718e1cf9ee0a23b1818fb301dbde1d
rohit <rohitkumarjan18@gmail.com>
2024-04-23 23:51:19 +0530

    hello rohit

--- src/main/java/org/allpracticeFile/org/practice/App.java
+++ src/main/java/org/allpracticeFile/org/practice/App.java
@@ -4,10 +4,8 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("changes ----1");
-    System.out.println("changes ----2");
-    System.out.println("changes ----3");
-    System.out.println("changes ----4");
-    System.out.println("changes ----5");
-    System.out.println("changes ----6");
+    System.out.println("changes ----1");
+    System.out.println("changes ----1");
+  
   }
 }
