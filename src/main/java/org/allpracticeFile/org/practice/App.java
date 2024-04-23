a19ab56270567458485c31b53c3c8f47dd50e061
rohit <rohitkumarjan18@gmail.com>
2024-04-23 23:38:04 +0530

    commit the app class changes

--- src/main/java/org/allpracticeFile/org/practice/App.java
+++ src/main/java/org/allpracticeFile/org/practice/App.java
@@ -3,5 +3,8 @@ package org.allpracticeFile.org.practice;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("changes ----1");
+    System.out.println("changes ----2");
+    System.out.println("changes ----3");
   }
 }
